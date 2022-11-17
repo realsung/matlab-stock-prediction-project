9195078a2e55ccb9016032d2c2acc3e57b942d29
gmlwns0704 <84502133+gmlwns0704@users.noreply.github.com>
2022-11-17 16:56:20 +0900

    하루단위로

--- trade_price_regression/price_trade_regression.m
+++ trade_price_regression/price_trade_regression.m
@@ -1,7 +1,7 @@
 date = datetime(date, 'InputFormat', 'yyyy/MM/dd');
-date = date(day(date) == 1);
-trade = trade(day(date) == 1);
-price = price(day(date) == 1);
+%date = date(day(date) == 1);
+%trade = trade(day(date) == 1);
+%price = price(day(date) == 1);
 
 figure()
 hold on
@@ -10,3 +10,5 @@ scatter(trade, price);
 xlabel('trade');
 ylabel('price');
 title('주가-거래량 회귀분석');
+
+hold off
